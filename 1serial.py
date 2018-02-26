02356447206dda6ef3654c5ac89038c17aaae490
7avenged <adityamohanjuit@gmail.com>
2018-02-26 23:47:12 +0530

    New files added

---
+++ 1serial.py
@@ -0,0 +1,21 @@
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+#csvfile = open('data_log1.csv', 'wb') #open file for operation
+#writer = csv.writer(csvfile) 
+
+ser = serial.Serial("/dev/ttyACM4", 9600)
+#ser = serial.Serial("/dev/rfcomm0", 9600)
+a=0 
+while a!=120:
+    print ser.readline()    
+    #k = ser.readline()
+    #sound,smoke = k.split(",")
+    #angle = float(angle)
+          
+    #writer.writerow([sound, smoke])
+              
+    a = a+1
+
